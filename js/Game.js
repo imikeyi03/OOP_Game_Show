1f9d2af4d3a8baa51dcb1de03df288c4b09570cf
mikey mann <mikey.mannx3@gmail.com>
2020-08-24 12:57:58 -0700

    Game phrases creation

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,26 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+    this.missed = 0;
+    this.phrases = this.createPhrases();
+    this.activePhrase = null;
+   }
+   /**
+     * Creates phrases for use in game
+     * @return {array} An array of phrases that could be used in the game
+     */
+
+   // Creates phrases for use in game
+    createPhrases() {
+        let phrases = ['Hello little girl','Kato did it','Is this dress small or am I fat','tiggle bitty','I declare bankruptcy'];
+
+        phrases = phrases.map((str) => (new Phrase(str)));
+        return phrases;
+    };
+}
+
+
+
--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+//  Temp code to test class instances of phrase and game
+
+const game = new Game();
+
+game.phrases.forEach((phrases, index) => {
+    console.log(`Phrase ${index} - phrase: ${phrases.phrase}`);
+});
